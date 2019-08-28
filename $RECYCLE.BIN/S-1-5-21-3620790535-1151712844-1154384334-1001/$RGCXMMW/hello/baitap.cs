8fb0651639fe0626f0a6e242e58cd385f4ebbce2
tranthanhha070619 <you@example.com>
2019-08-28 20:38:29 +0700

    ghghjgjhkwgmf

---
+++ $RECYCLE.BIN/S-1-5-21-3620790535-1151712844-1154384334-1001/$RGCXMMW/hello/baitap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hello
+{
+    class baitap
+    {
+         static void Main(String[] args)
+        {
+            string number = "qwead";
+
+            int count = 0;
+
+        beginLoop:
+
+            Console.WriteLine(number);
+            count ++;
+            if(count < 10)
+            goto beginLoop;
+
+            Console.WriteLine("weq");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ $RECYCLE.BIN/S-1-5-21-3620790535-1151712844-1154384334-1001/$RPETT0O/Cau_Truc_Lenh/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cau_Truc_Lenh
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("time");
+            //Console.WriteLine(Console.ReadLine());
+            Console.ReadKey();
+            Console.ReadKey(false);
+            Console.ReadKey(true);
+        }
+    }
+}
